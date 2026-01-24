c48768a33a5b106f2b47b53388323d8943e8af50
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-01-24 13:12:18 +0100

    Add footer

--- assets/image-randomizer.js
+++ assets/image-randomizer.js
@@ -1,15 +1,15 @@
 // Image data used for random background selection
 const imgData = [
     {
-        credits: 'Photo by <a class="text-white-50" target="_blank" href="https://unsplash.com/@introspectivedsgn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erik Mclean</a> on <a class="text-white-50" target="_blank" href="https://unsplash.com/photos/a-body-of-water-surrounded-by-mountains-on-a-foggy-day-nF_826lMv6s?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>',
+        credits: 'Photo by <a class="text-white-50 opacity-75" target="_blank" href="https://unsplash.com/@introspectivedsgn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erik Mclean</a> on <a class="text-white-50" target="_blank" href="https://unsplash.com/photos/a-body-of-water-surrounded-by-mountains-on-a-foggy-day-nF_826lMv6s?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>',
         imageUrl: './assets/img/erik-mclean-nF_826lMv6s-unsplash_opt.jpg'
     },
     {
-        credits: 'Photo by <a class="text-white-50" target="_blank" href="https://unsplash.com/@jslate01?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joshua Slate</a> on <a class="text-white-50" target="_blank" href="https://unsplash.com/photos/green-trees-surrounded-by-clouds-HN9O6R5wUUc?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>' ,
+        credits: 'Photo by <a class="text-white-50 opacity-75" target="_blank" href="https://unsplash.com/@jslate01?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joshua Slate</a> on <a class="text-white-50" target="_blank" href="https://unsplash.com/photos/green-trees-surrounded-by-clouds-HN9O6R5wUUc?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>' ,
         imageUrl: './assets/img/joshua-slate-HN9O6R5wUUc-unsplash_opt.jpg'
     },
     {
-        credits: 'Photo by <a class="text-white-50" target="_blank" href="https://unsplash.com/@mmmtan7?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Tandian</a> on <a class="text-white-50" target="_blank" href="https://unsplash.com/photos/a-body-of-water-with-mountains-in-the-background-yncB2zEUnOo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>' ,
+        credits: 'Photo by <a class="text-white-50 opacity-75" target="_blank" href="https://unsplash.com/@mmmtan7?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Tandian</a> on <a class="text-white-50" target="_blank" href="https://unsplash.com/photos/a-body-of-water-with-mountains-in-the-background-yncB2zEUnOo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>' ,
         imageUrl: './assets/img/michael-tandian-yncB2zEUnOo-unsplash_opt.jpg'
     }
 ]
